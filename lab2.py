4f8d997d65aad16ead4343709b582e5b311c6ffb
AlexKrasnov <ya.sasha-krasnov@yandex.ru>
2020-02-11 16:47:29 +0300

    upgrade lab2

--- lab2.py
+++ lab2.py
@@ -4,22 +4,22 @@ class Convolution3D():
     def __init__(self, kernel):
         self.kernel = kernel
 
-    def control(point, shape):
-        return 0 <= point[0] < shape[0] and 0 <= point[1] < shape[1]
+    def control(point, range):
+        return 0 <= point[0] < range[0] and 0 <= point[1] < range[1]
         
     def clamp (value, min, max):
         return np.maximum(np.minimum(value, max), min)
 
     def calculation(self, input_matrix):
-        w, h, d = self.kernel.shape
-        W, H = input_matrix.shape
+        w, h, K = self.kernel.shape
+        W, H, K = input_matrix.shape
         
         output_matrix = np.zeros((W, H), dtype="uint8")
         
         for x, y in np.ndindex(W, H):
             pixel = 0
             
-            for x_, y_, z_ in np.ndindex(w, h, d):
+            for x_, y_, z_ in np.ndindex(w, h, K):
                 tx = x - w // 2 + x_
                 ty = y - h // 2 + y_
 
